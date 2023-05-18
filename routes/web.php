ab7deaf0c62cbbdb127d3863ee2bceada4ecc39b
claudemarjean <marjean.8888@gmail.com>
2023-05-18 09:52:54 +0300

    affichage de mon premier articles

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $articles = [
+            [
+                'title' => "mon premier article",
+                'content' => 'Si vous avez déjà un dépôt Git local configuré pour votre projet et que vous souhaitez le pousser vers un dépôt distant (repository) sur GitHub, vous pouvez suivre ces étape'
+            ],
+            [
+                'title' => "mon deuxième article",
+                'content' => 'Si vous avez déjà un dépôt Git local configuré pour votre projet et que vous souhaitez le pousser vers un dépôt distant (repository) sur GitHub, vous pouvez suivre ces étape'
+            ],
+            [
+                'title' => "mon troisième article",
+                'content' => 'Si vous avez déjà un dépôt Git local configuré pour votre projet et que vous souhaitez le pousser vers un dépôt distant (repository) sur GitHub, vous pouvez suivre ces étape'
+            ]
+        ];
 
-Route::get('hello-world',function(){
-    return view('hello-world');
+
+    return view('home',[
+        'articles' => $articles
+    ]);
 });
